7f23e156c98b482b0826c68729bc814daaff2856
yuvi123cpu <76250633+yuvi123cpu@users.noreply.github.com>
2026-08-06 22:44:07 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (45.22%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+
+        int pos = haystack.find(needle);
+
+        if(pos == string::npos)
+        return -1;
+
+        return pos;
+        
+    }
+};
