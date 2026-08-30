e1afe76e471e72c04f84e09a23f01c14437aefe0
yuvi123cpu <76250633+yuvi123cpu@users.noreply.github.com>
2026-08-30 19:53:32 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (71.18%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> lis;
+
+        for(int x : nums)
+        {
+            auto i = lower_bound(lis.begin(),lis.end(),x);
+
+            if(i == lis.end())
+            lis.push_back(x);
+
+            else
+            *i = x;
+        }
+        return lis.size();
+        
+    }
+};
